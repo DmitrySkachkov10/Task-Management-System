1394195f72032e44224cf7df4c92b12bfa3ba8ae
DmitrySkachkov10 <dima.dmitry06@gmail.com>
2024-08-06 19:28:01 +0300

    create main user dto class

---
+++ user-service/src/main/java/by/dmitry_skachkov/userservice/core/dto/UserDTO.java
@@ -0,0 +1,15 @@
+package by.dmitry_skachkov.userservice.core.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UserDTO {
+
+    private String email;
+
+    private String password;
+}
