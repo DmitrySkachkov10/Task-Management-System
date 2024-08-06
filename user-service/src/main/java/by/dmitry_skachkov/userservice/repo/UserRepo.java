aa6fe7c0fb7d0b36e9cfa7072bf0e6a95ff783b9
DmitrySkachkov10 <dima.dmitry06@gmail.com>
2024-08-06 18:00:45 +0300

    create project structure

---
+++ user-service/src/main/java/by/dmitry_skachkov/userservice/repo/UserRepo.java
@@ -0,0 +1,8 @@
+package by.dmitry_skachkov.userservice.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository {
+}
