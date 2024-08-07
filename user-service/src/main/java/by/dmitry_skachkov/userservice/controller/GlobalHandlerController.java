7ec35561044e6a35e17154ffda81a74cb3b3c3ca
DmitrySkachkov10 <dima.dmitry06@gmail.com>
2024-08-07 14:57:22 +0300

    added RestControllerAdvice

---
+++ user-service/src/main/java/by/dmitry_skachkov/userservice/controller/GlobalHandlerController.java
@@ -0,0 +1,38 @@
+package by.dmitry_skachkov.userservice.controller;
+
+
+import by.dmitryskachkov.dto.ExcepionResponseDto;
+import by.dmitryskachkov.exception.exceptions.ValidationException;
+import by.dmitryskachkov.exception.exceptions.email.EmailAlreadyExistsException;
+import by.dmitryskachkov.exception.exceptions.email.InvalidEmailFormatException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice(annotations = RestController.class)
+public class GlobalHandlerController {
+
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<ExcepionResponseDto> defaultExceptionHandler(ValidationException e) {
+        HttpStatus status;
+
+        if (e instanceof EmailAlreadyExistsException) {
+            status = HttpStatus.CONFLICT;
+        } else if (e instanceof InvalidEmailFormatException) {
+            status = HttpStatus.BAD_REQUEST;
+        } else {
+            status = HttpStatus.BAD_REQUEST;
+        }
+
+        return new ResponseEntity<>(new ExcepionResponseDto(e.getMessage()), status);
+    }
+
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ExcepionResponseDto> defaultExceptionHandler(RuntimeException e) {
+        ExcepionResponseDto responseDto = new ExcepionResponseDto("An unexpected error occurred");
+        return new ResponseEntity<>(responseDto, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
