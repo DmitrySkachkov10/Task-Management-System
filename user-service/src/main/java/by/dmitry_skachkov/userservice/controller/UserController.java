75048edb54e804bc4a34cc80e307789188dcb3e6
DmitrySkachkov10 <dima.dmitry06@gmail.com>
2024-08-07 14:59:21 +0300

    added UserController.java

---
+++ user-service/src/main/java/by/dmitry_skachkov/userservice/controller/UserController.java
@@ -0,0 +1,31 @@
+package by.dmitry_skachkov.userservice.controller;
+
+import by.dmitry_skachkov.userservice.core.dto.UserDTO;
+import by.dmitry_skachkov.userservice.service.api.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/registrate")
+    public ResponseEntity<Void> registrate(@RequestBody UserDTO userDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> logIn(@RequestBody UserDTO userDTO) {
+        return ResponseEntity.ok(userService.logIn(userDTO));
+    }
+}
