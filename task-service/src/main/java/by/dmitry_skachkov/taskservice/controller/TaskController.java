a6729a609d6c27334b4d320a0c36ab05ac7bf911
DmitrySkachkov10 <dima.dmitry06@gmail.com>
2024-08-09 12:59:42 +0300

    updated controller

--- task-service/src/main/java/by/dmitry_skachkov/taskservice/controller/TaskController.java
+++ task-service/src/main/java/by/dmitry_skachkov/taskservice/controller/TaskController.java
@@ -1,8 +1,9 @@
 package by.dmitry_skachkov.taskservice.controller;
 
+import by.dmitry_skachkov.taskservice.core.dto.task.PageOfTask;
 import by.dmitry_skachkov.taskservice.core.dto.task.TaskCreateDto;
-import by.dmitry_skachkov.taskservice.core.dto.task.UpdateTaskDto;
 import by.dmitry_skachkov.taskservice.service.TaskServiceImpl;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,32 +22,36 @@ public class TaskController {
     @PostMapping("/create")
     public ResponseEntity<?> createTask(@RequestBody TaskCreateDto task) {
         taskService.create(task);
-        return null;
+        return new ResponseEntity<>("Task has been created", HttpStatus.CREATED);
     }
 
-    @GetMapping("/my")
-    public ResponseEntity<?> readMy(@RequestParam(name = "page", defaultValue = "1") int page,
-                                    @RequestParam(name = "size", defaultValue = "5") int size) {
-        return ResponseEntity.ok().body(taskService.getMyTasks(page, size));
+    @GetMapping()
+    public ResponseEntity<PageOfTask> getTasks(@RequestParam(name = "page", defaultValue = "1") int page,
+                                               @RequestParam(name = "size", defaultValue = "5") int size,
+                                               @RequestParam(name = "uuid", required = false) UUID uuid) {
+        return ResponseEntity.ok().body(taskService.getByUserUuid(uuid, page, size));
     }
 
-    @DeleteMapping("/{uuid}")
+    @DeleteMapping("/delete/{uuid}")
     public ResponseEntity<?> delete(@PathVariable UUID uuid) {
         taskService.delete(uuid);
         return ResponseEntity.ok().build();
     }
 
-    @PutMapping("/update")
-    public ResponseEntity<?> updateMyTask(@RequestBody UpdateTaskDto updateTaskDto) {
-        taskService.updateTask(updateTaskDto);
+    @PutMapping("/update/{uuid}/version/{version}")
+    public ResponseEntity<?> updateMyTask(@RequestBody TaskCreateDto taskCreateDto,
+                                          @PathVariable long version,
+                                          @PathVariable UUID uuid) {
+        taskService.updateTask(taskCreateDto, version, uuid);
         return ResponseEntity.ok().build();
     }
 
-    @PutMapping("/update/{uuid}/version/{version}")
+    @PutMapping("/update/status/{uuid}/version/{version}")
     private ResponseEntity<?> updateStatus(@PathVariable UUID uuid,
                                            @PathVariable long version,
                                            @RequestParam String status) {
         taskService.changeStatus(status, version, uuid);
         return ResponseEntity.ok().build();
     }
+
 }
