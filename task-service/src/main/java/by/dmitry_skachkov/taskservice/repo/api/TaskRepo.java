152c22f2fa2a4c863d7e94031ae9f3ed38c19758
DmitrySkachkov10 <dima.dmitry06@gmail.com>
2024-08-09 12:59:01 +0300

    add method in repository

--- task-service/src/main/java/by/dmitry_skachkov/taskservice/repo/api/TaskRepo.java
+++ task-service/src/main/java/by/dmitry_skachkov/taskservice/repo/api/TaskRepo.java
@@ -1,7 +1,11 @@
 package by.dmitry_skachkov.taskservice.repo.api;
 
 import by.dmitry_skachkov.taskservice.repo.entity.Task;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
@@ -9,7 +13,8 @@ import java.util.UUID;
 @Repository
 public interface TaskRepo extends JpaRepository<Task, UUID> {
 
-
-
+    @Query("SELECT t FROM Task t LEFT JOIN t.taskPerformers tp " +
+            "WHERE t.authorUuid = :userUuid OR tp.performerUuid = :userUuid")
+    Page<Task> findByAuthorUuidOrPerformerUuid(@Param("userUuid") UUID userUuid, Pageable pageable);
 
 }
